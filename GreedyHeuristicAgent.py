837993ef27471b581d671fb290443f46b296f4b3
Chen <kensh1nhualuoweiyang@gmail.com>
2022-11-18 19:01:46 -0500

    Positional Heuristic

--- GreedyHeuristicAgent.py
+++ GreedyHeuristicAgent.py
@@ -2,6 +2,63 @@ import random
 
 
 valueMapping = {"K":0, "P":1,"N":3,"B":3,"R":5,"Q":9}
+
+knightScore = [[1,1,1,1,1,1,1,1],
+                [1,2,2,2,2,2,2,1],
+                [1,2,3,3,3,3,2,1],
+                [1,2,3,4,4,3,2,1],
+                [1,2,3,4,4,3,2,1],
+                [1,2,3,3,3,3,2,1],
+                [1,2,2,2,2,2,2,1],
+                [1,1,1,1,1,1,1,1]]
+
+whitePawnScore = [[8,8,8,8,8,8,8,8],
+                  [8,8,8,8,8,8,8,8],
+                  [5,6,6,7,7,6,6,5],
+                  [2,3,3,5,5,3,3,2],
+                  [1,2,3,4,4,3,2,1],
+                  [1,1,2,3,3,2,1,1],
+                  [1,1,1,1,1,1,1,1],
+                  [0,0,0,0,0,0,0,0]]
+blackPawnScore = [[0,0,0,0,0,0,0,0],
+                  [1,1,1,1,1,1,1,1],
+                  [1,1,2,3,3,2,1,1],
+                  [1,2,3,4,4,3,2,1],
+                  [2,3,3,5,5,3,3,2],
+                  [5,6,6,7,7,6,6,5],
+                  [8,8,8,8,8,8,8,8],
+                  [8,8,8,8,8,8,8,8],]
+bishopScore = [[4,3,2,1,1,2,3,4],
+                [3,4,3,2,2,3,4,3],
+                [2,3,4,3,3,4,3,2],
+                [1,2,3,4,4,3,2,1],
+                [1,2,3,4,4,3,2,1],
+                [2,3,4,3,3,4,3,2],
+                [3,4,3,2,2,3,4,3],
+                [4,3,2,1,1,2,3,4]]
+rookScore =[[4,3,4,4,4,4,3,4],
+            [4,4,4,4,4,4,4,4],
+            [1,1,2,3,3,2,1,1],
+            [1,2,3,4,4,3,2,1],
+            [1,2,3,4,4,3,2,1],
+            [1,1,2,3,3,2,1,1],
+            [4,4,4,4,4,4,4,4],
+            [4,3,4,4,4,4,3,4]]
+
+queenScore =[[1,1,1,1,1,1,1,1],
+             [1,2,3,3,3,1,1,1],
+             [1,4,3,3,3,4,2,1],
+             [1,2,3,3,3,2,2,1],
+             [1,2,3,3,3,2,2,1],
+             [1,4,3,3,3,4,2,1],
+             [1,2,3,3,3,1,1,1],
+             [1,1,1,1,1,1,1,1]]
+
+
+
+
+piecePositionScore = {"N":knightScore,"Q":queenScore,"R":rookScore,"B":bishopScore,"bP":blackPawnScore,"wP":whitePawnScore}
+
 CHECKMATE = 1000
 STALEMATE = 0
 
@@ -99,4 +156,30 @@ def evaluateBoard(state):
             elif col[0] == "b":
                 result -= valueMapping[col[1]]
     return result
-            
+
+
+def evaluateBoardWithPosition(state):
+    if state.checkMate:
+        if state.whiteToMove:
+            return -CHECKMATE
+        return CHECKMATE
+    elif state.staleMate:
+        return 0
+
+    result = 0
+    for row in range(len(state.board)):
+        for col in range(len(state.board[row])):
+            piece = state.board[row][col]
+            if piece != "--":
+                positionScore = 0
+                if piece[1] != "K":
+                    if piece[1] == "P":
+                        positionScore = piecePositionScore[piece][row][col]
+                    else:
+                        positionScore = piecePositionScore[piece[1]][row][col]
+                if piece[0] == "w":
+                    result += valueMapping[col[1]] + positionScore * 0.1
+                elif piece[0] == "b":
+                    result -= valueMapping[col[1]] + positionScore * 0.1
+
+    return result
